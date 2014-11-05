f2d354e0b28c643de3434151e88291698e71a219
Braden Anderson <bradencanderson@gmail.com>
2014-11-04 16:33:02 -0800

    sass in gulpfile

--- web/gulpfile.js
+++ web/gulpfile.js
@@ -1,19 +1,23 @@
+var autoprefix = require('gulp-autoprefixer');
 var browserify = require('browserify');
 var del = require('del');
 var gulp = require('gulp');
+var notify = require('gulp-notify');
 var reactify = require('reactify');
+var sass = require('gulp-ruby-sass');
 var source = require('vinyl-source-stream');
 
 var paths = {
   app_jsx: ['./src/js/app.jsx'],
-  js: ['src/js/*.js'],
+  js: ['./src/js/*.js'],
+  sass: ['./src/sass/*.scss'],
 };
 
 gulp.task('clean', function(done) {
   del(['build'], done);
 });
 
-gulp.task('js', ['clean'], function() {
+gulp.task('js', function() {
   browserify(paths.app_jsx)
     .transform(reactify)
     .bundle()
@@ -23,6 +27,19 @@ gulp.task('js', ['clean'], function() {
 
 gulp.task('watch', function() {
   gulp.watch(paths.js, ['js']);
+  gulp.watch(paths.sass, ['css']);
 });
 
-gulp.task('default', ['watch', 'js']);
+gulp.task('css', function() {
+  return gulp.src(paths.sass)
+    .pipe(sass({
+        style: 'nested',
+        loadPath: ['./src/sass']
+      })
+      .on("error", notify.onError(function(error) {
+        return "Error: " + error.message;
+      })))
+    .pipe(gulp.dest('./build/'));
+});
+
+gulp.task('default', ['clean', 'watch', 'js', 'css']);
