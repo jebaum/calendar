771b663f694160e2027db8f6cf92a7e0d39aba50
Seena Burns <hello@seenaburns.com>
2014-11-05 13:12:15 -0800

    Move requesting events to function, add display_daily_term outline, clamp seconds since epoch

--- cli.py
+++ cli.py
@@ -1,4 +1,4 @@
-import sys, json
+import sys, json, datetime
 import requests
 
 class Event(object):
@@ -21,13 +21,56 @@ class Event(object):
 
         return json.dumps(json_dict)
 
+    def clamp_seconds_since_epoch(self, sec):
+        sec_string = str(sec)
+        if len(sec_string) > 10:
+            return int(sec_string[:10])
+        elif len(sec_string) < 10:
+            return int(sec_string.ljust(10, '0'))
+        return sec_string
+
     def load_from_json(self, json_dict):
         for k,v in json_dict.items():
             setattr(self, k, v)
 
+        # Adjust startTime / endTime to ensure in seconds since epoch
+        self.startTime = self.clamp_seconds_since_epoch(self.startTime)
+        self.endTime = self.clamp_seconds_since_epoch(self.endTime)
 
-r = requests.get('http://localhost:4567/date_start/%s/date_end/%s' % (sys.argv[1], sys.argv[2]))
-for item in r.json():
-    e = Event()
-    e.load_from_json(item)
-    print(e)
+        self.startDate = datetime.datetime.fromtimestamp(self.startTime)
+        self.endDate = datetime.datetime.fromtimestamp(self.endTime)
+
+def get_events(startTime, endTime):
+    """
+    Make request for events between start and end time, convert to event class
+    and return as a list of events
+    """
+    url = 'http://localhost:4567/date_start/%s/date_end/%s' % (startTime, endTime)
+    r = requests.get(url)
+    events = []
+    for event in r.json():
+       e = Event()
+       e.load_from_json(event)
+       events.append(e)
+
+    return events
+
+def display_daily_term(events):
+    """
+    Display today's events, printed to stdout
+    Attempt to match orgmode style
+    """
+    for i in range(0,24):
+        hour = "%s:00" % (i)
+        print(hour.rjust(5, ' '), '-'*10)
+
+    for e in events:
+        print(e.startDate, e.endDate)
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print("Usage: python cli.py <start> <end>")
+        sys.exit(1)
+
+    events = get_events(sys.argv[1], sys.argv[2])
+    display_daily_term(events)
