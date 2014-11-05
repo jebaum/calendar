1605123c4ed3be02b6394edc520046f434168c1c
Seena Burns <hello@seenaburns.com>
2014-11-05 12:49:58 -0800

    Begin Event class, loading from json and to string functionality

--- cli.py
+++ cli.py
@@ -1,5 +1,33 @@
-import sys
+import sys, json
 import requests
 
+class Event(object):
+    def __init__(self):
+        self.title = None
+        self.startTime = 0
+        self.endTime = 0
+        self.location = None
+        self.category = None
+        self.description = None
+        self.json_keys = ["title", "startTime", "endTime", "location", "category", "description"]
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __str__(self):
+        json_dict = {}
+        for k in self.json_keys:
+            json_dict[k] = getattr(self, k)
+
+        return json.dumps(json_dict)
+
+    def load_from_json(self, json_dict):
+        for k,v in json_dict.items():
+            setattr(self, k, v)
+
+
 r = requests.get('http://localhost:4567/date_start/%s/date_end/%s' % (sys.argv[1], sys.argv[2]))
-print(r.json())
+for item in r.json():
+    e = Event()
+    e.load_from_json(item)
+    print(e)
