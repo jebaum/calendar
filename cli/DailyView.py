bc33daab03258c5522622ca2e3c9414757cbdbf0
Seena Burns <hello@seenaburns.com>
2014-12-03 13:27:29 -0800

    DailyView inherits from TimeView

--- cli/DailyView.py
+++ cli/DailyView.py
@@ -1,8 +1,11 @@
 import curses
 from ListView import *
+from TimeView import *
 
-class DailyView(ListView):
+class DailyView(TimeView,ListView):
   def __init__(self, EventStore):
+    # super init intializes in reverse order of multiple inheritance seemingly
+    # e.g. Class(C1, C2) is init'ed in C2, C1 order
     super(DailyView, self).__init__()
 
     self.event_lists = []
