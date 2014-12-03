4146800eeb31ea061c684d24029978122bb89599
Seena Burns <hello@seenaburns.com>
2014-12-03 12:31:12 -0800

    Refactor ListView, extract drawing functions to make inheritance easier

--- cli/ListView.py
+++ cli/ListView.py
@@ -12,46 +12,75 @@ class ListView(ScrollableView):
     self.box_header_width = 0
 
   def set_content_size(self, w, h):
+    # Width defined by argument
     self.content_w = w
-    self.content_h = 1 + len(self.event_lists) + sum([self.height_for_event_list(x) for x in self.event_lists])
 
+    # Height based on list size
+    top_border_h = 1
+    separator_border_h = 1
+    item_heights = [self.height_for_event_list(x)+separator_border_h for x in self.event_lists]
+    self.content_h = top_border_h + sum(item_heights)
+
+    # Resize pad based on content, mark as needed for update
     self.pad.resize(self.content_h,self.content_w+1)
     self.needs_update = True
 
-  def update_size(self):
-    self.set_content_size(self.content_w,self.content_h)
-
   def add_event(self, i, e):
     self.event_lists[i].append(e)
-    self.update_size()
+    # Update height
+    self.set_content_size(self.content_w,0)
 
   def height_for_event_list(self, el):
     return max(1,2 * len(el))
 
+  def update(self):
+    self.draw_top_border()
+
+    yoff = 1
+    l = len(self.event_lists)
+    for i in range(l):
+      self.draw_box(yoff, self.event_lists[i])
+      if i != l-1:
+        self.draw_separator(yoff+self.height_for_event_list(self.event_lists[i]))
+      yoff += self.height_for_event_list(self.event_lists[i])+1
+
+    self.draw_bottom_border()
+
+  # 
+  # DRAWING FUNCTIONS
+  #
   def draw_box(self, y, events):
     h = self.height_for_event_list(events)
     xoff = self.box_header_width + 1
     
+    # Draw side borders
+    self.draw_item_border(y,h)
+
     # Draw header
     self.draw_box_header(y)
 
+    # Draw events
+    for i in range(len(events)):
+      self.draw_event(y+i*2,xoff,events[i])
+
+  def draw_box_header(self,y):
+    pass
+
+  def draw_event(self,y,x,e):
+      self.pad.addstr(y,x+1, str(e.hour).rjust(2,'0') + ":" + str(e.minute).rjust(2,'0'))
+      self.pad.addstr(y,x+7,str(e.title))
+      self.pad.addstr(y+1,x+7,str(e.description))
+
+  #
+  # BORDER FUNCTIONS
+  #
+  def draw_item_border(self,y,h):
     # Right vertical line
     for i in range(h):
       self.pad.addch(y+i,0,curses.ACS_VLINE)
     # Left vertical line
     for i in range(h):
       self.pad.addch(y+i, self.content_w-1,curses.ACS_VLINE)
-
-    # Draw events
-    for i in range(len(events)):
-      e = events[i]
-      # Start time 
-      self.pad.addstr(y+i*2,xoff+1, str(e.hour).rjust(2,'0') + ":" + str(e.minute).rjust(2,'0'))
-      self.pad.addstr(y+i*2,xoff+7,str(e.title))
-      self.pad.addstr(y+1+i*2,xoff+7,str(e.description))
-
-  def draw_box_header(self,y):
-    pass
 
   def draw_separator(self,y):
     self.pad.addch(y,0,curses.ACS_LTEE)
@@ -72,16 +101,3 @@ class ListView(ScrollableView):
     self.pad.addch(y,self.content_w-1,curses.ACS_LRCORNER)
     for i in range(1,self.content_w-1):
       self.pad.addch(y,i,curses.ACS_HLINE)
-
-  def update(self):
-    self.draw_top_border()
-
-    yoff = 1
-    l = len(self.event_lists)
-    for i in range(l):
-      self.draw_box(yoff, self.event_lists[i])
-      if i != l-1:
-        self.draw_separator(yoff+self.height_for_event_list(self.event_lists[i]))
-      yoff += self.height_for_event_list(self.event_lists[i])+1
-
-    self.draw_bottom_border()
