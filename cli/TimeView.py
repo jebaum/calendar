6daa0645a4e1def5745ef539bc063fc548fdd092
Seena Burns <hello@seenaburns.com>
2014-12-03 15:04:44 -0800

    Time scrolling for day, month and week

--- cli/TimeView.py
+++ cli/TimeView.py
@@ -9,28 +9,31 @@ class TimeView(ScrollableView):
     super(TimeView, self).__init__()
     self.view_header = "Init"
 
-    self.header_format = "%B %d %Y, %H:%M:%S"
-    self.periodSize = datetime.timedelta(days = 1)
     self.periodOffset = 0
+
+    # Options for subclasses
+    self.header_format = "%B %d %Y, %H:%M:%S"
+    self.get_period_func = self.get_month_period
+    self.offset_period_func = self.apply_month_offset
 
   @property
   def now(self):
     return datetime.datetime.now()
 
-  def get_view_header(self):
-    return self.get_period()[0].strftime(self.header_format)
+  @property
+  def period(self):
+    return self.offset_period_func(self.get_period_func(self.now))
 
-  def get_period(self):
-    # Return period adjusted with self.periodOffset
-    start_dt,end_dt = self.get_day_period(self.now)
-    start_dt += self.periodOffset*self.periodSize
-    end_dt += self.periodOffset*self.periodSize
-    return (start_dt,end_dt)
+  def get_view_header(self):
+    return self.period[0].strftime(self.header_format) + " - " + self.period[1].strftime(self.header_format)
 
   def change_time_period(self, amount):
     self.periodOffset += amount
     self.needs_update = True
 
+  # 
+  # Get period contatining dt methods
+  #
   def get_day_period(self, dt):
     # 00:00 to 24:00
     start_dt = dt - datetime.timedelta(hours = dt.hour, minutes = dt.minute, seconds = dt.second)
@@ -41,7 +44,7 @@ class TimeView(ScrollableView):
     # Sunday to Saturday
     start_day_dt = self.get_day_period(dt)[0]
     start_dt = start_day_dt - datetime.timedelta(days = start_day_dt.weekday()+1)
-    end_dt = start_dt + datetime.timedelta(days = 6)
+    end_dt = start_dt + datetime.timedelta(days = 7)
     return (start_dt,end_dt)
 
   def get_month_period(self, dt):
@@ -49,4 +52,38 @@ class TimeView(ScrollableView):
     start_day_dt = self.get_day_period(dt)[0]
     start_dt = start_day_dt - datetime.timedelta(days = start_day_dt.day - 1)
     end_dt = start_dt + datetime.timedelta(days = calendar.monthrange(start_day_dt.year, start_day_dt.month)[1])
+    return (start_dt,end_dt)
+
+  #
+  # Apply period offset methods
+  # 
+  def apply_day_offset(self, periodTuple):
+    periodSize = datetime.timedelta(days = 1)
+    start_dt,end_dt = periodTuple
+    start_dt += self.periodOffset*periodSize
+    end_dt += self.periodOffset*periodSize
+    return (start_dt,end_dt)
+
+  def apply_week_offset(self, periodTuple):
+    periodSize = datetime.timedelta(days = 7)
+    start_dt,end_dt = periodTuple
+    start_dt += self.periodOffset*periodSize
+    end_dt += self.periodOffset*periodSize
+    return (start_dt,end_dt)
+
+  def apply_month_offset(self, periodTuple):
+    start_dt,end_dt = periodTuple
+    sign = lambda x: 1 if x >= 0 else -1
+
+    if self.periodOffset >= 0:
+      for i in range(abs(self.periodOffset)):
+        start_dt += datetime.timedelta(days = calendar.monthrange(start_dt.year, (start_dt.month-1)%12+1)[1])
+        end_dt += datetime.timedelta(days = calendar.monthrange(end_dt.year, (end_dt.month-1)%12+1)[1])
+    else:
+      for i in range(abs(self.periodOffset)):
+        start_dt -= datetime.timedelta(days = calendar.monthrange(start_dt.year, (start_dt.month-2)%12+1)[1])
+        end_dt -= datetime.timedelta(days = calendar.monthrange(end_dt.year, (end_dt.month-2)%12+1)[1])
+
+    # start_dt += self.periodOffset*periodSize
+    # end_dt += self.periodOffset*periodSize
     return (start_dt,end_dt)
