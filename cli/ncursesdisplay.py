ce2e6e392306915ba92699d2f8979667f4e7009f
Seena Burns <hello@seenaburns.com>
2014-12-03 16:18:53 -0800

    Create WeeklyView

--- cli/ncursesdisplay.py
+++ cli/ncursesdisplay.py
@@ -7,6 +7,7 @@ import random
 
 from ListView import *
 from DailyView import *
+from WeeklyView import *
 from SummaryView import *
 from CommandView import *
 from event import *
@@ -55,18 +56,17 @@ class NcursesDisplay:
 
       es = EventStore()
       self.DailyView = DailyView(es)
+      self.WeeklyView = WeeklyView(es)
+
+      self.ListView = self.WeeklyView
       self.SummaryView = SummaryView()
       self.CommandView = CommandView()
       self.resize_views()
 
-      self.DailyView.display()
-      self.SummaryView.display()
-      self.CommandView.display()
-
       global resize_flag
 
       while True:
-        self.DailyView.display()
+        self.ListView.display()
         self.SummaryView.display()
         self.CommandView.display()
         # curses.doupdate()
@@ -83,17 +83,17 @@ class NcursesDisplay:
           break
         # WASD Scrolling
         elif c == ord('a'):
-          # self.DailyView.scroll_x(-1)
-          self.DailyView.change_time_period(-1)
+          # self.ListView.scroll_x(-1)
+          self.ListView.change_time_period(-1)
         elif c == ord('d'):
-          # self.DailyView.scroll_x(1)
-          self.DailyView.change_time_period(1)
+          # self.ListView.scroll_x(1)
+          self.ListView.change_time_period(1)
         elif c == ord('s'):
-          self.DailyView.scroll_y(1)
+          self.ListView.scroll_y(1)
         elif c == ord('w'):
-          self.DailyView.scroll_y(-1)
+          self.ListView.scroll_y(-1)
         elif c == ord('r'):
-          self.DailyView.add_event(random.randint(0,19), Event())
+          self.ListView.add_event(random.randint(0,19), Event())
 
         # time.sleep(0.01)
 
@@ -106,12 +106,12 @@ class NcursesDisplay:
       sys.exit(1)
 
   def resize_views(self):
-      self.DailyView.set_view_position(0,0)
-      self.DailyView.set_view_size(self.w-1,self.h*2/3-1)
-      self.DailyView.set_content_size(self.w,self.h)
+      self.ListView.set_view_position(0,0)
+      self.ListView.set_view_size(self.w-1,self.h*2/3-1)
+      self.ListView.set_content_size(self.w,self.h)
 
-      self.SummaryView.set_view_position(0,self.DailyView.view_h+1)
-      self.SummaryView.set_view_size(self.w-1,self.h-self.DailyView.view_h-3)
+      self.SummaryView.set_view_position(0,self.ListView.view_h+1)
+      self.SummaryView.set_view_size(self.w-1,self.h-self.ListView.view_h-3)
       self.SummaryView.set_content_size(self.w,(self.h-1)*2/3)
 
       self.CommandView.set_view_position(0,self.h-1)
