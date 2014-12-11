de98b1fe2cb8515a00c4b2ee68e7fe7cc3f42890
James Baumgarten <jebaum@ucla.edu>
2014-12-10 22:19:53 -0800

    make calendar file parsing more robust

--- src/yamlcal/calendar/Server.java
+++ src/yamlcal/calendar/Server.java
@@ -39,53 +39,80 @@ public class Server {
     // API endpoints, status codes, jackson annotations, etc. fix this
     public static void main(String[] args) throws JsonProcessingException, FileNotFoundException, Exception {
 
-        // http://yaml-online-parser.appspot.com/
+        // create objects to parse calendar file
         InputStream input = new FileInputStream(new File("calendar.yaml"));
         Yaml calReader    = new Yaml();
 
+        // read in the calendar file as a java object
+        // NOTE: unchecked type conversion error is a false positive, results from how snakeyaml is designed
         @SuppressWarnings("unchecked")
         Map<String, List<Map<String, String>>> eventList = (Map<String, List<Map<String, String>>>) calReader.load(input);
 
+
+        // create the list that we will populate with calendar Events
         // TODO: duplicate keys to the map may cause problems? two different events with the same name?
+        // TODO: add recur handling?
         List<Event> calendarList = new ArrayList<Event>();
+
+        // go through each event in the calendar file, using the event title as the "index"
         for (String eventTitleString : eventList.keySet()) {
             Title eventTitle = new Title(eventTitleString);
 
+            // the actual structure of the parsed calendar file, as seen above, is triply nested, so this is needed to extract the data
+            // each attribute of an event (with the exception of titles, which are keys), e.g. location, description, category, etc, is in its own one element map
+            // the key of the map is the attribute name
+            // eventAttributeList is a list of these maps that we'll go through in order to determine the attribute values
             List<Map<String, String>> eventAttributeList = eventList.get(eventTitleString);
             Map<String, String> eventAttributeMap        = new HashMap<String, String>();
             for (Map<String, String> eventAttribute : eventAttributeList) {
-                eventAttributeMap.putAll(eventAttribute);
+                eventAttributeMap.putAll(eventAttribute); // gather all attributes into a single map to make things easier
             }
 
+            // pure text attributes that don't have to be parsed
             Location eventLocation       = new Location(eventAttributeMap.get("location"));
             Description eventDescription = new Description(eventAttributeMap.get("description"));
             Category eventCategory       = new Category(eventAttributeMap.get("category"));
 
+            // get the raw text of the time, which can be in one of two different formats
             String eventStartTimeString = eventAttributeMap.get("start");
             StartTime eventStartTime    = null;
-            // TODO: need a more robust way to parse dates, or at least need to allow for not specifying time?
-            if (eventStartTimeString != null) {
+            if (eventStartTimeString != null) { // it's possible that no start time was specified, in which case we won't include it in our calendarList
                 DateFormat formatter = new SimpleDateFormat("MM/dd/yy h:mm a");
-                Date date            = (Date)formatter.parse(eventStartTimeString);
-                eventStartTime       = new StartTime(date);
+                Date date;
+                try {
+                    date = (Date)formatter.parse(eventStartTimeString);
+                } catch (ParseException e) {
+                    formatter = new SimpleDateFormat("MM/dd/yy");
+                    date      = (Date)formatter.parse(eventStartTimeString);
+                }
+                eventStartTime = new StartTime(date);
             }
 
+            // same code for end time
+            // NOTE: it bothers me that this is virtually identical code, and the way I'm using try-catch is smelly
+            // unfortunately I couldn't think of a better design, and this Works for valid calendar files, so it'll do for now
             String eventEndTimeString = eventAttributeMap.get("end");
             EndTime eventEndTime      = null;
             if (eventStartTime != null) {
                 DateFormat formatter = new SimpleDateFormat("MM/dd/yy h:mm a");
-                Date date            = (Date)formatter.parse(eventEndTimeString);
-                eventEndTime         = new EndTime(date);
+                Date date;
+                try {
+                    date = (Date)formatter.parse(eventEndTimeString);
+                } catch (ParseException e) {
+                    formatter = new SimpleDateFormat("MM/dd/yy");
+                    date      = (Date)formatter.parse(eventEndTimeString);
+                }
+                eventEndTime = new EndTime(date);
             }
 
+            // now that we've gotten all the data for the event, create the Event object and add it to our calendar list
             Event someEvent = new Event(eventTitle, eventLocation, eventDescription, eventCategory, eventStartTime, eventEndTime);
             calendarList.add(someEvent);
-            System.out.println("Event: " + someEvent + " added to calendar list");
+            System.err.println("Event: " + someEvent + " added to calendar list");
         }
 
-
         ObjectMapper mapper = new ObjectMapper();
-        System.out.println(mapper.writeValueAsString(calendarList));
+        System.err.println(mapper.writeValueAsString(calendarList));
 
         /*
          * BEGIN ENDPOINTS
